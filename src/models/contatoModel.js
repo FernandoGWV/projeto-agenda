9c1e76dcc9c6bc07451c745e0e49dc0d8810f93d
Luiz Fernando <luizwinther09@gmail.com>
2022-12-25 16:24:46 -0300

    read e delete da parte dos contatos.

--- src/models/contatoModel.js
+++ src/models/contatoModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const { async } = require('regenerator-runtime')
 const validator = require('validator')
 const ContatoSchema = new mongoose.Schema({
   nome: {type: String, required: true},
@@ -18,11 +19,7 @@ function Contato(body){
 }
 
 
-Contato.buscaPorId = async function(id){
-  if(typeof id !== 'string') return;
-const user = await ContatoModel.findById(id)
-return user
-}
+
 
 Contato.prototype.register = async function(){
   this.valida()
@@ -60,4 +57,34 @@ this.body = {
 
 }
 
+
+Contato.prototype.edit  = async function(id){
+  if(typeof id !== 'string') return 
+  this.valida()
+  if(this.errors.length
+   > 0) return ;
+
+this.contato = await ContatoModel.findByIdAndUpdate(id, this.body, {new:true})
+}
+
+// metodos estaticos
+Contato.buscaPorId = async function(id){
+  if(typeof id !== 'string') return;
+const contato = await ContatoModel.findById(id)
+return contato
+}
+
+Contato.buscaContatos = async function(){
+const contatos = await ContatoModel.find()
+.sort({
+  criadoEm: -1
+})
+return contatos
+}
+
+Contato.delete = async function(id){
+  if(typeof id !== 'string') return;
+const contato = await ContatoModel.findOneAndDelete({_id: id})
+return contato
+}
 module.exports = Contato
